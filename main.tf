a69ffb4b167d75b7565aa346dd595aef4c0c7188
Markus Hartmann <markush1986@gmail.com>
2019-04-05 19:25:12 +0200

    added jenkins

--- main.tf
+++ main.tf
@@ -147,6 +147,26 @@ resource "helm_release" "traefik" {
   depends_on = ["kubernetes_cluster_role_binding.tiller"]
 }
 
+resource "helm_release" "jenkins" {
+  name  = "jenkins"
+  chart = "stable/jenkins"
+
+  set {
+    name  = "Master.ServiceType"
+    value = "ClusterIP"
+  }
+
+  set {
+    name  = "Master.ingress.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "Master.ingress.hostName"
+    value = "jenkins.${var.domain}"
+  }
+}
+
 output "kubeconfig" {
   value = "${digitalocean_kubernetes_cluster.trainingscenter.kube_config.0.raw_config}"
 }
