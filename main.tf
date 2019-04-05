e9576826e764e807a1f0b54af10b6c552d139578
Johannes Siebel <johannes.siebel@gmail.com>
2019-04-05 21:12:32 +0200

    Fixed some typos and some general stuff

--- main.tf
+++ main.tf
@@ -31,13 +31,12 @@ provider "digitalocean" {
 resource "digitalocean_kubernetes_cluster" "trainingscenter" {
   name    = "trainingscenter"
   region  = "fra1"
-  version = "1.13.5-do.1"
-  tags    = ["staging"]
+  version = "1.13.5-do.0"
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-2vcpu-2gb"
-    node_count = 3
+    size       = "s-1vcpu-2gb"
+    node_count = 1
   }
 }
 
