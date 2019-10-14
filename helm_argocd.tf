d365744b8dd5fe6e3ba4c074ce65d83363b0fcce
Johannes Siebel <johannes.siebel@gmail.com>
2019-10-14 20:14:53 +0200

    Saving current state of work
    Signed-off-by: Johannes Siebel <johannes.siebel@gmail.com>

--- helm_argocd.tf
+++ helm_argocd.tf
@@ -1,5 +1,17 @@
 # ref: https://github.com/argoproj/argo-helm/tree/master/charts/argo-cd
-# data "helm_repository" "argo" {
-#   name = "argo"
-#   url  = "https://argoproj.github.io/argo-helm"
-# }
+data "helm_repository" "argo" {
+  name = "argo"
+  url  = "https://argoproj.github.io/argo-helm"
+}
+
+#
+# ref: https://github.com/terraform-providers/terraform-provider-helm/issues/102
+resource "helm_release" "argocd" {
+  name  = "argocd"
+  chart = "argo/argo-cd"
+  version = "0.5.4"
+
+  values = [file("values-argocd.yml")]
+
+  depends_on = [kubernetes_cluster_role_binding.tiller]
+}
